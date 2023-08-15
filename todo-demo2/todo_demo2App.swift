a55c590de3a216573c7702ee12b51e92e4a71cd7
Alisona <alisonahle@gmail.com>
2023-08-15 10:33:22 -0400

    Initial Commit

---
+++ todo-demo2/todo_demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  todo_demo2App.swift
+//  todo-demo2
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct todo_demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
